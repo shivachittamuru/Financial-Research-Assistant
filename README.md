cc5cbf2cc601efa7bc06962ac6fffaa34bb053dc
ShivaKumarChittamuru <shchitt@microsoft.com>
2024-06-03 14:52:04 -0700

    Adding initial working code

---
+++ README.md
@@ -0,0 +1,39 @@
+## Prerequisites for running the solution:
+
+Clone the repo:
+```console
+git clone https://github.com/shivachittamuru/Financial-Research-Assistant.git
+cd Financial-Research-Assistant
+```
+
+Create a conda environment:
+```console
+conda create --name assistant python=3.10
+conda activate assistant
+```
+
+Install `PIP` requirements
+```console
+pip install -r requirements.txt
+```
+
+Update the values in `.env-sample` and rename it to `.env`.
+
+
+## Polygon.ai API
+
+Go to `https://polygon.io/`, and create a free API key. Add the key in .env file
+
+
+## LangGraph
+
+LangGraph is a library that enables the creation of stateful applications involving multiple actors (like users, agents, or characters) interacting with Large Language Models (LLMs). It allows developers to define actors, attributes, relationships, and behaviors using a graph-based representation. It’s an extension of the LangChain library for coordinating multiple chains or actors in a cyclic computational process.
+
+Get started with this intro tutorial - `https://langchain-ai.github.io/langgraph/tutorials/introduction/`
+
+
+
+
+
+
+
---
+++ app/app.py
@@ -0,0 +1,81 @@
+import os
+from langchain import hub
+from langchain.agents import create_openai_functions_agent
+from langchain_openai.chat_models import AzureChatOpenAI
+from langchain_community.utilities.polygon import PolygonAPIWrapper
+from langchain_community.tools import PolygonLastQuote, PolygonTickerNews, PolygonFinancials, PolygonAggregates
+import gradio as gr
+
+import os, openai
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+os.environ["LANGCHAIN_PROJECT"] = "langgraph-financial-agent"
+
+
+llm  = AzureChatOpenAI(
+    api_key=os.environ["AZURE_OPENAI_API_KEY"],
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+    azure_deployment="gpt-4-32k",
+    model="gpt-4-32k",
+    streaming=True,
+    temperature=0
+)
+
+prompt = hub.pull("hwchase17/openai-functions-agent")
+
+polygon = PolygonAPIWrapper()
+tools = [
+    PolygonLastQuote(api_wrapper=polygon),
+    PolygonTickerNews(api_wrapper=polygon),
+    PolygonFinancials(api_wrapper=polygon),
+    PolygonAggregates(api_wrapper=polygon)
+]
+
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.agents import AgentFinish
+
+# Define the agent
+agent_runnable = create_openai_functions_agent(llm, tools, prompt)
+agent = RunnablePassthrough.assign(
+    agent_outcome = agent_runnable
+)
+
+# Define the function to execute tools
+def execute_tools(data):
+    agent_action = data.pop('agent_outcome')
+    tool_to_use = {t.name: t for t in tools}[agent_action.tool]
+    observation = tool_to_use.invoke(agent_action.tool_input)
+    data['intermediate_steps'].append((agent_action, observation))
+    return data
+
+# Define logic that will be used to determine which conditional edge to go down
+def should_continue(data):
+    if isinstance(data['agent_outcome'], AgentFinish):
+        return "exit"
+    else:
+        return "continue"
+    
+# Define the Graph with nodes and edges
+from langgraph.graph import END, Graph
+
+workflow = Graph()
+workflow.add_node("agent", agent)
+workflow.add_node("tools", execute_tools)
+workflow.set_entry_point("agent")
+workflow.add_conditional_edges(
+    "agent",
+    should_continue,
+    {
+        "continue": "tools",
+        "exit": END
+    }
+)
+workflow.add_edge('tools', 'agent')
+app = workflow.compile()
+
+result = app.invoke({"input": "Give me the top news about SBUX stock?", "intermediate_steps": []})
+output = result['agent_outcome'].return_values["output"]
+print(output)
